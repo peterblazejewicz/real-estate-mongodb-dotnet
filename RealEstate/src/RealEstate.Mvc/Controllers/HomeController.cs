42d5bb7a133964e475886927d4837b3c1e6cfcce
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-08-21 22:11:56 +0200

    Controllers implementation cont.

--- RealEstate/src/RealEstate.Mvc/Controllers/HomeController.cs
+++ RealEstate/src/RealEstate.Mvc/Controllers/HomeController.cs
@@ -1,16 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 
 namespace RealEstate.Mvc.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public static RealEstateContext Context = new RealEstateContext();
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var buildInfoCommand = new BsonDocument("buildinfo", 1);
+            var buildInfo = await Context.Database.RunCommandAsync<BsonDocument>(buildInfoCommand);
+            return Content(buildInfo.ToJson(), "application/json");
         }
 
         public IActionResult Error()
--- RealEstate/src/RealEstate.Mvc/Controllers/RentalsController.cs
+++ RealEstate/src/RealEstate.Mvc/Controllers/RentalsController.cs
@@ -1,22 +1,58 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using RealEstate.Mvc.Model;
 
 namespace RealEstate.Mvc.Controllers
 {
     public class RentalsController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        public readonly RealEstateContext Context = new RealEstateContext();
+        public async Task<ActionResult> Index(RentalsFilter filters)
         {
-            return View();
+            var rentals = await FilterRentals(filters)
+              .Select(r => new RentalViewModel
+            {
+              Id = r.Id,
+                  Address = r.Address,
+                  Description = r.Description,
+                  NumberOfRooms = r.NumberOfRooms,
+                  Price = r.Price
+            })
+            .OrderBy(r => r.Price)
+              .ThenByDescending(r => r.NumberOfRooms)
+              .ToListAsync();
+            var model = new RentalsList
+          {
+            Rentals = rentals,
+                Filters = filters
+          };
+          return View(model);
         }
 
         public IActionResult Error()
         {
             return View();
+        }
+
+        private IMongoQueryable<Rental> FilterRentals(RentalsFilter filters)
+        {
+            var rentals = Context.Rentals.AsQueryable();
+
+            if (filters.MinimumRooms.HasValue)
+          {
+            rentals = rentals
+                  .Where(r => r.NumberOfRooms >= filters.MinimumRooms);
+          }
+
+          if (filters.PriceLimit.HasValue)
+          {
+            rentals = rentals
+                  .Where(r => r.Price <= filters.PriceLimit);
+          }
+          return rentals;
         }
     }
 }
