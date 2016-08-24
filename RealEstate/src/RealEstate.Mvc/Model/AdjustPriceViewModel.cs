32bc2efb0dbab89f1174813e7b712f166f3def2a
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-08-24 23:44:32 +0200

    Complex model for AdjustPrice view

---
+++ RealEstate/src/RealEstate.Mvc/Model/AdjustPriceViewModel.cs
@@ -0,0 +1,8 @@
+namespace RealEstate.Mvc.Model
+{
+    public class AdjustPriceViewModel
+    {
+        public Rental Rental { get; set; }
+        public AdjustPrice AdjustPrice { get; set; }
+    }
+}
